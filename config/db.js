d25406dd05728ac9f090c7ffaedd31f7d8c44baf
Sruthi Mallireddy <sruthimallireddy76@gmail.com>
2026-01-09 17:33:11 +0530

    updated mongoose connection

--- config/db.js
+++ config/db.js
@@ -1,11 +1,9 @@
+
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
@@ -14,3 +12,4 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
--- server.js
+++ server.js
@@ -19,3 +19,4 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
