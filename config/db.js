b805236dbedf9d85b02b53611616ffeaba27909b
Sruthi Mallireddy <sruthimallireddy76@gmail.com>
2026-01-09 17:26:46 +0530

    updated mongodb connections

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
-    process.exit(1); // terminate app if DB fails
+    process.exit(1);
   }
 };
 
